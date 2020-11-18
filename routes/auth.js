452d4873ac2bfc4bfc791480730f91549a091bcf
Idode Kerobo <idode.kerobo@gmail.com>
2020-11-17 21:27:35 -0700

    moving firebase service account to env var

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,19 @@ const router = express.Router();
 require('dotenv').config();
 
 const admin = require("firebase-admin");
-// const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-// const serviceAccount = require("/Users/idodekerobo/Documents/baby mogul/Young Zuckerberg/dash-7174b-firebase-adminsdk-n0t08-2548cc1118.json");
-const serviceAccount = require('../serviceAccountKey.json');
 admin.initializeApp({
-   credential: admin.credential.cert(serviceAccount),
+   credential: admin.credential.cert({
+      "type": process.env.FIREBASE_ACCT,
+      "project_id": process.env.FIREBASE_ACCT_PROJ_ID,
+      "private_key_id": process.env.FIREBASE_ACCT_PRIV_KEY_ID,
+      "private_key": process.env.FIREBASE_ACCT_PRIV_KEY.replace(/\\n/g, '\n'),
+      "client_email": process.env.FIREBASE_ACCT_CLIENT_EMAIL,
+      "client_id": process.env.FIREBASE_ACCT_CLIENT_ID,
+      "auth_uri": process.env.FIREBASE_ACCT_AUTH_URI,
+      "token_uri": process.env.FIREBASE_ACCT_TOKEN_URI,
+      "auth_provider_x509_cert_url": process.env.FIREBASE_ACCT_AUTH_PROVIDER,
+      "client_x509_cert_url": process.env.FIREBASE_ACCT_CLIENT_X509_CERT
+   }),
    databaseURL: "https://dash-7174b.firebaseio.com"
 });
 
