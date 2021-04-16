b65ff0da5eb7fd54ef339ce82ebf64abb6e3e4ca
Idode Kerobo <idode.kerobo@gmail.com>
2021-04-16 10:54:14 -0500

    fixing success/refresh/cancel stripe url's

--- routes/customer-sign-up.js
+++ routes/customer-sign-up.js
@@ -29,10 +29,8 @@ router.post('/signup-rhemi', async (req, res) => {
          // the actual Session ID is returned in the query parameter when your customer is redirected to the success page.
          // TODO - need to direct them to a page to set up their stripe account here
          // success_url: 'https://example.com/success.html?session_id={CHECKOUT_SESSION_ID}', 
-         success_url: 'https://localhost:5000/', 
-         // TODO - direct them to the homepage here
-         // cancel_url: 'https://example.com/canceled.html',
-         cancel_url: 'https://localhost:5000/',
+         success_url: 'https://www.rhemi.io/signup-stripe', 
+         cancel_url: 'https://www.rhemi.io/pricing',
       });
       res.send({
          sessionId: session.id,
@@ -55,8 +53,8 @@ router.post('/signup-user-stripe-acct', async (req, res) => {
 
       const accountLink = await stripe.accountLinks.create({
          account: account.id,
-         refresh_url: 'https://www.rhemi.co', // give refresh url if user doesn't complete
-         return_url: 'https://www.rhemi.co', // return url when finished
+         refresh_url: 'https://www.rhemi.io/signup-stripe', // give refresh url if user doesn't complete
+         return_url: 'https://www.rhemi.io/thank-you', // return url when finished
          type: 'account_onboarding',
       });
 
